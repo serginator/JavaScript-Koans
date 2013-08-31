14d16a7c0e1afcefc5c1bab275fee7ffbc12c301
Sergio Ruiz <serginator@gmail.com>
2013-08-30 20:07:59 -0400

    GREEN: About Scope; RED: About Regilar Expressions

--- topics/about_scope.js
+++ topics/about_scope.js
@@ -3,7 +3,7 @@ module("About Scope (topics/about_scope.js)");
 thisIsAGlobalVariable = 77;
 
 test("global variables", function() {
-    equals(thisIsAGlobalVariable, __, 'is thisIsAGlobalVariable defined in this scope?');
+    equals(thisIsAGlobalVariable, 77, 'is thisIsAGlobalVariable defined in this scope?');
 });
 
 test("variables declared inside of a function", function() {
@@ -12,10 +12,10 @@ test("variables declared inside of a function", function() {
     // this is a self-invoking function. Notice that it calls itself at the end ().
     (function() {
         var innerVariable = "inner";
-        equals(outerVariable, __, 'is outerVariable defined in this scope?');
-        equals(innerVariable, __, 'is innerVariable defined in this scope?');
+        equals(outerVariable, "outer", 'is outerVariable defined in this scope?');
+        equals(innerVariable, "inner", 'is innerVariable defined in this scope?');
     })();
 
-    equals(outerVariable, __, 'is outerVariable defined in this scope?');
-    equals(typeof(innerVariable), __, 'is innerVariable defined in this scope?');
+    equals(outerVariable, "outer", 'is outerVariable defined in this scope?');
+    equals(typeof(innerVariable), "undefined", 'is innerVariable defined in this scope?');
 });
